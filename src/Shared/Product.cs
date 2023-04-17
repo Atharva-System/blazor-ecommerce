bda559ccf90a37fc1ea89d0c3e986a79474fe648
Rahul Patel <er.rahulpatel@live.com>
2023-04-17 22:27:06 +0530

    Initial-commit

---
+++ src/Shared/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorEcommerce.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
